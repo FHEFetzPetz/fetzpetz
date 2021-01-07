77400155e2ba2bc49039b9dad18981127b873c6a
Luca Voges <vogeslu@gmail.com>
2021-01-07 09:00:35 +0100

    Addex examples for Controller, Styles and more

--- src/controller/IndexController.php
+++ src/controller/IndexController.php
@@ -11,12 +11,25 @@ class IndexController extends Controller
     public function shareRoutes(): array
     {
         return [
-            '/' => 'index'
+            '/' => 'index',
+            '/test' => 'test'
         ];
     }
 
     public function index() {
         $this->setParameter("title", "FetzPetz | Main Page");
+
+        $this->addExtraHeaderFields([
+            ["type" => "stylesheet", "href" => "/assets/css/mainpage.css"]
+        ]);
+
         $this->setView("index.php");
+    }
+
+    public function test() {
+        $this->setParameter("title", "Es ist Mittwoch (meine Freunde)");
+        $this->setParameter("navigation", false);
+
+        $this->setView("test.php");
     }
 }
--- views/index.php
+++ views/index.php
@@ -1,7 +1,1 @@
-<h1>Icons</h1>
-
-Bars Icon: <i class="icon bars"></i><br>
-Search Icon: <i class="icon search"></i><br>
-Shopping Cart Icon: <i class="icon shopping-cart"></i><br>
-User Icon: <i class="icon user"></i><br>
-Heart Icon: <i class="icon heart"></i><br>
+<div id="mainpage">Mainpage</div>
