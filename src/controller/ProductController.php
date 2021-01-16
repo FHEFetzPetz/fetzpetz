bb95f051715118702852c5949173402ba199547f
Munchkin129 <dirk.hofmann@fh-erfurt.de>
2021-01-16 13:27:13 +0100

    added productController, added example images

---
+++ src/controller/ProductController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\FetzPetz\Controller;
+
+use App\FetzPetz\Components\Controller;
+use App\FetzPetz\Model\Product;
+use App\FetzPetz\Model\User;
+
+class ProductController extends Controller
+{
+
+    public function shareRoutes(): array
+    {
+        return [
+            '/product' => 'product',
+        ];
+    }
+
+    public function product() {
+        $this->setParameter("title", "FetzPetz | Product Page");
+
+        $this->addExtraHeaderFields([
+            ["type" => "stylesheet", "href" => "/assets/css/product.css"]
+        ]);
+
+        $product = $this->kernel->getModelService()->findOne(Product::class);
+
+        $this->setParameter("product", $product);
+
+        $this->setView("product.php");
+    }
+}
