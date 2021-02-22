5af825a3af3acba1867645d8a83e7366dd5080f9
Munchkin129 <dirk.hofmann@fh-erfurt.de>
2021-02-22 14:38:03 +0100

    fixed search

--- src/controller/SearchController.php
+++ src/controller/SearchController.php
@@ -3,7 +3,9 @@
 namespace App\FetzPetz\Controller;
 
 use App\FetzPetz\Components\Controller;
+use App\FetzPetz\Model\Category;
 use App\FetzPetz\Model\Product;
+use App\FetzPetz\Model\ProductCategory;
 
 class SearchController extends Controller
 {
@@ -22,9 +24,13 @@ class SearchController extends Controller
 
         $database = $this->kernel->getDatabase();
 
-        $queryString = 'SELECT * FROM ' . Product::TABLENAME . ' WHERE active = 1 AND (
-            name LIKE \'%' . $query . '%\' OR
-            description LIKE \'%' . $query . '%\')';
+        $queryString = 'SELECT *, p.name as name, p.description as description, p.id as id FROM ' . Product::TABLENAME . ' as p 
+            LEFT JOIN ' . ProductCategory::TABLENAME . ' as pc ON pc.product_id = p.id 
+            LEFT JOIN ' . Category::TABLENAME . ' as c ON c.id = pc.category_id WHERE p.active = 1 AND (
+            p.name LIKE \'%' . $query . '%\' OR
+            p.description LIKE \'%' . $query . '%\' OR
+            c.name LIKE \'%' . $query . '%\' OR
+            c.description LIKE \'%' . $query . '%\')';
 
         $result = [];
         $products = [];
